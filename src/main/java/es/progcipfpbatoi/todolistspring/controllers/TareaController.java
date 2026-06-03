9dc80b8b8a625535c8edf6c7cd0d539ba9fef729
Pablo-Cabezas <pabcabser@alu.edu.gva.es>
2026-06-03 13:41:21 +0200

    corregir parametros de codigo

--- src/main/java/es/progcipfpbatoi/todolistspring/controllers/TareaController.java
+++ src/main/java/es/progcipfpbatoi/todolistspring/controllers/TareaController.java
@@ -80,7 +80,7 @@ public class TareaController {
 
     // Muestra todos los datos de una tarea concreta.
     @GetMapping("/tarea-detail")
-    public String tareaDetailActionView(@RequestParam int codigo, Model model) {
+    public String tareaDetailActionView(@RequestParam("codigo") int codigo, Model model) {
         try {
             model.addAttribute("tarea", tareaRepository.get(codigo));
             return "tarea_detail_view";
@@ -95,7 +95,7 @@ public class TareaController {
 
     // Elimina una tarea usando su codigo y luego muestra un mensaje al usuario.
     @GetMapping("/tarea-delete")
-    public String tareaDeleteActionView(@RequestParam int codigo, Model model) {
+    public String tareaDeleteActionView(@RequestParam("codigo") int codigo, Model model) {
         try {
             Tarea tarea = tareaRepository.delete(codigo);
             model.addAttribute("titulo", "Tasca eliminada");
